31206c04fa34cdfba929237332ed50ba86d5cb02
Tarang Patel <tarangp@me.com>
2013-11-03 16:46:43 +0200

    Updates

--- lib/telismo.rb
+++ lib/telismo.rb
@@ -1,5 +1,6 @@
 require "telismo/version"
 require "rest_client"
+require "base64"
 
 module Telismo
   # Your code goes here...
@@ -18,14 +19,29 @@ module Telismo
   		"http://#{self.api_key}:@#{@api_base}#{method}"
   	end
 
+  	def self.process_callback(request)
+  		JSON.parse request.body.read
+  	end
+
   	class Calls
   		def self.create(params)
 
-  			url = Telismo.api_url 'test'
+  			url = Telismo.api_url 'create'
 
-  			response = RestClient.post url, params
 
-  			puts response.to_str
+  			response = RestClient.post url, {
+  				:number => params[:number],
+  				:name => params[:name],
+  				:instruction => {
+  					:text => params[:text],
+  					:sample => params[:sample]
+  				},
+  				:fields_b64 => Base64.strict_encode64(params[:fields].to_json),
+  				:type => params[:type],
+  				:callback => params[:callback]
+  			}
+
+  			JSON.parse(response.to_str)
   		end
   	end
 end
